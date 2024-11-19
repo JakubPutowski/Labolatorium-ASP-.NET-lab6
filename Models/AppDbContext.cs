c1d2b76b5aba1995977e4894d93d6344f8590273
jakub.putowski <jakub.putowski@microsoft.wsei.edu.pl>
2024-11-19 12:23:07 +0100

    lab7

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.General;
 
 namespace Project.Models;
 
-public class AppDbContext: DbContext
+public class AppDbContext: IdentityDbContext<IdentityUser>
 {
     public DbSet<ContactEntity> Contacts {
         get;
@@ -27,6 +30,65 @@ public class AppDbContext: DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+        string ADMIN_ID = Guid.NewGuid().ToString();
+        string USER_ID = Guid.NewGuid().ToString();
+
+        modelBuilder.Entity<IdentityRole>().HasData(
+            new IdentityRole()
+            {
+                Id = ADMIN_ID,
+                Name = "admin",
+                NormalizedName = "ADMIN",
+                ConcurrencyStamp = ADMIN_ID
+            },
+            new IdentityRole()
+            {
+                Id = USER_ID,
+                Name = "user",
+                NormalizedName = "USER",
+                ConcurrencyStamp = USER_ID
+            }
+        );
+        var admin = new IdentityUser()
+        {
+            Id = ADMIN_ID,
+            UserName = "Adam",
+            NormalizedUserName = "ADAM",
+            Email = "adam@wsei.edu.pl",
+            NormalizedEmail = "ADAM@WSEI.EDU.PL",
+            EmailConfirmed = true
+        };
+        var user = new IdentityUser()
+        {
+            Id = USER_ID,
+            UserName = "Kuba",
+            NormalizedUserName = "KUBA",
+            Email = "kuba@wsei.edu.pl",
+            NormalizedEmail = "KUBA@WSEI.EDU.PL",
+            EmailConfirmed = true
+        };
+        PasswordHasher<IdentityUser> hasher = new PasswordHasher<IdentityUser>();
+        admin.PasswordHash = hasher.HashPassword(admin, "1234!");
+        user.PasswordHash = hasher.HashPassword(user, "1234@");
+
+        modelBuilder.Entity<IdentityUser>().HasData(admin, user);
+
+        modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+        new IdentityUserRole<string>()
+        {
+            RoleId = ADMIN_ID,
+            UserId = ADMIN_ID
+        },
+        new IdentityUserRole<string>()
+        {
+            RoleId = USER_ID,
+            UserId = USER_ID
+        }
+        
+        );
+        
+        
         modelBuilder.Entity<OrganizationEntity>()
             .OwnsOne(o => o.Address)
             .HasData(
--- obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cab729997dea13219f124881dd57d326a147159")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11c43e95263be669debea1d9ea88e378e254c27b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
