43ca10f4a5662ec44f65520b7b42dbeeb3e422e6
Jakub Putowski <jakub.putowski@microsoft.wsei.edu.pl>
2024-11-20 01:00:29 +0100

    lab3

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.Models;
+
+namespace Project.Controllers;
+
+public class ContactController : Controller
+{
+    static private Dictionary<int, ContactModel> _contacts= new Dictionary<int, ContactModel>()
+    {
+        {1, new() {Id = 1, Email = "email@wsei.com",FirstName = "Jakub", LastName = "Putowski", BirthDate = new DateTime(1990, 11,05), PhoneNumber = "111 111 111"}},
+        {2, new() {Id = 2, Email = "email1@wsei.com",FirstName = "Karol", LastName = "Kowal", BirthDate = new DateTime(1950, 03,17), PhoneNumber = "222 222 222"}}
+    };
+
+    private static int currentID = 3;
+    // Lista kontaktów
+    public IActionResult Index()
+    {
+        return View(_contacts);
+    }
+
+    public ActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Add(ContactModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        model.Id = ++currentID;
+        _contacts.Add(model.Id, model);
+        return View("Index", _contacts);
+    }
+
+    public ActionResult Delete(int id)
+    {
+        _contacts.Remove(id);
+        return View("Index", _contacts);
+    }
+
+    public ActionResult Details(int id)
+    {
+        return View(_contacts[id]);
+    }
+}
--- obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+405a406c3fdc370a44d27b774ef684651de3ce44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
