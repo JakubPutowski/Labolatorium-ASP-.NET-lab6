d069916a2cc51d7deb098c91e0de3807b183ced5
Jakub Putowski <jakub.putowski@microsoft.wsei.edu.pl>
2024-11-20 01:54:23 +0100

    lab5

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project.Models;
+
+public class AppDbContext:DbContext
+{
+    public DbSet<ContactEntity> Contacts
+    {
+        get;
+        set;
+    }
+    private string DbPath { get; set; }
+    public AppDbContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "contacts.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data source={DbPath}");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ContactModel>().HasData(
+            new ContactEntity()
+            {
+                Id = 1,
+                FirstName = "Jakub",
+                LastName = "Kowalski",
+                PhoneNumber = "123456789",
+                BirthDate = new DateTime(2000, 10, 10),
+                Email = "jakub@wsei.edu.pl",
+                Created = DateTime.Now,
+            },
+            new ContactEntity()
+            {
+                Id = 2,
+                FirstName = "Karol",
+                LastName = "Kowalski",
+                PhoneNumber = "132556789",
+                BirthDate = new DateTime(1990, 11, 15),
+                Email = "karol@wsei.edu.pl",
+                Created = DateTime.Now,
+            }
+        );
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
+using Project.Models;
 using Project.Models.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddSingleton<iContactService, MemoryContactService>();
 
 var app = builder.Build();
